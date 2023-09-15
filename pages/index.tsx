dca684a844c1ccd00ab1a3cb76f3568a4d35682a
Alex <user@user.com>
2023-09-16 00:10:29 +0300

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+// import Image from 'next/image';
+
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Главная</title>
+        <meta name="description" content="" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main>
+        <div>Hello !</div>
+      </main>
+    </>
+  )
+}
